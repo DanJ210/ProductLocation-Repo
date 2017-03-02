3335de702a21d44c986d17bca8954e77226ac978
djackson <djackson@ecnetwork.com>
2017-03-02 17:06:08 -0500

    Added model and section for javascript

---
+++ ProductLocations-Draft2-Empty/src/ProductLocations-Draft2-Empty/Models/TableCell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductLocations_Draft2_Empty.Models {
+    public class TableCell {
+        public int Id { get; set; }
+        public string ElementName { get; set; }
+        public string ServerName { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string TableNumber { get; set; }
+        public string ColumnNumber { get; set; }
+        public string ProductName { get; set; }
+    }
+}
