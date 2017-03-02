8d130c86f51e337dc52a4f5ed2749aeb9811f19e
djackson <djackson@ecnetwork.com>
2017-03-02 17:02:36 -0500

    Added a basic controller

--- ProductLocations-Draft2-Empty/src/ProductLocations-Draft2-Empty/Startup.cs
+++ ProductLocations-Draft2-Empty/src/ProductLocations-Draft2-Empty/Startup.cs
@@ -7,15 +7,23 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
-namespace ProductLocations_Draft2_Empty
-{
-    public class Startup
-    {
+namespace ProductLocations_Draft2_Empty {
+    public class Startup {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
+
+        //public Startup(IHostingEnvironment env) {
+        //    var builder = new ConfigurationBuilder()
+        //        .SetBasePath(env.ContentRootPath);
+
+        //    Configuration = builder.Build();
+        //}
+        //public IConfigurationRoot Configuration { get; }
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +36,17 @@ namespace ProductLocations_Draft2_Empty
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+            
+
+            app.UseMvc(routes => {
+            routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
