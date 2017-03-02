004e1e3085b37733f04a82ea3ee1399d7618f337
djackson <djackson@ecnetwork.com>
2017-03-02 17:13:55 -0500

    Playing with controller and a model.

--- ProductLocations-Draft2-Empty/src/ProductLocations-Draft2-Empty/Controllers/HomeController.cs
+++ ProductLocations-Draft2-Empty/src/ProductLocations-Draft2-Empty/Controllers/HomeController.cs
@@ -3,16 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProductLocations_Draft2_Empty.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ProductLocations_Draft2_Empty.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace ProductLocations_Draft2_Empty.Controllers {
+    public class HomeController : Controller {
         // GET: /<controller>/
-        public IActionResult Index()
-        {
+        public IActionResult Index() {
+            var model = new TableCell() {
+                Id = 5,
+                ElementName = "Product0",
+                ServerName = "ECN-West1",
+                ColumnNumber = "1",
+                TableNumber = "0",
+                ProductName = "Product Name",
+                DateAdded = DateTime.Now
+            };
+            return Hey(model);
+        }
+
+        private IActionResult Hey(TableCell model2) {
             return Content("Hey");
         }
     }
